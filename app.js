38fb47f0fea7f8296b573f701b52f1f9f143c792
anushka kushwaha <anushkakushwaha2245@gmail.com>
2025-02-12 22:22:15 +0530

    main file

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+
+app.get("/",(req,res)=>{
+    res.send("Hello, i am root");
+});
 app.listen(8080,()=>{
     console.log("server is running on port 8080");
 })
