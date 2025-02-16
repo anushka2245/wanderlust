e69dc99de8c4e01333fada43ca22b8a6417d6dce
anushka kushwaha <anushkakushwaha2245@gmail.com>
2025-02-16 21:40:00 +0530

    updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const listing = require("./models/listing.js");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 main().then(()=>{
@@ -15,6 +16,21 @@ async function main(){
 
 app.get("/",(req,res)=>{
     res.send("Hello, i am root");
+});
+app.get("/testlisting",async (req,res)=>{
+    let sampleListing = new listing(
+        {
+        title:"my new villa",
+        description:"its a beautiful villa",
+        price:1000,
+        location:"bangalore",
+        image:"https://d3oo9u3p09egds.cloudfront.net/filters:format(webp)/rental_property/colina-villa-h/01_Facade__10_.jpeg",
+        country:"India"
+    }
+);
+    await sampleListing.save();
+    console.log("listing saved");
+    res.send("listing saved successful");
 });
 app.listen(8080,()=>{
     console.log("server is running on port 8080");
--- models/listing.js
+++ models/listing.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const listingSchema = new Schema({
     title:{
-        type: String
+        type: String,
+        required: true
     },
     description:{
         type:String
@@ -12,7 +13,10 @@ const listingSchema = new Schema({
         type:Number
     },
     image:{
-        type:String
+        type:String,
+        default:"https://media.istockphoto.com/id/119926339/photo/resort-swimming-pool.jpg?s=612x612&w=0&k=20&c=9QtwJC2boq3GFHaeDsKytF4-CavYKQuy1jBD2IRfYKc=",
+        set:(v)=>v===""?"https://media.istockphoto.com/id/119926339/photo/resort-swimming-pool.jpg?s=612x612&w=0&k=20&c=9QtwJC2boq3GFHaeDsKytF4-CavYKQuy1jBD2IRfYKc=":v,
+
     },
     location:{
         type:String
@@ -21,3 +25,5 @@ const listingSchema = new Schema({
         type:String
     }
 });
+const Listing = mongoose.model("Listing",listingSchema);
+module.exports = Listing;
