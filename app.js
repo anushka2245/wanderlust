89fdbbd0a389c4fed4a01e16c3c979e6e5ac27c1
anushka kushwaha <anushkakushwaha2245@gmail.com>
2025-02-18 22:37:11 +0530

    main file

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const listing = require("./models/listing.js");
 const path = require("path");
 
 
-app.set("view-engine","ejs")
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 main().then(()=>{
     console.log("connection to DB");
@@ -21,13 +23,24 @@ app.get("/",(req,res)=>{
     res.send("Hello, i am root");
 });
 
+//index route
 app.get("/listings",async (req,res)=>{
-   const alllistings = await listing.find({});
-   res.render("index.ejs",{alllistings});
+   const allListings = await listing.find({});
+   res.render("./listings/index.ejs",{allListings});
+});
+
+//show route
+app.get("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+    const Listing = await listing.findById(id);
+    res.render("./listings/show.ejs",{Listing});
 });
 
 
-
+//new route
+app.get("/listing/new",(req,res)=>{
+    
+});
 
 // app.get("/testlisting",async (req,res)=>{
 //     let sampleListing = new listing(
