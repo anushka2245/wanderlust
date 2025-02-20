57b1d288c018b697a4020f0e207d3f75657cf2fd
anushka kushwaha <anushkakushwaha2245@gmail.com>
2025-02-20 23:57:08 +0530

    main file

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const listing = require("./models/listing.js");
 const path = require("path");
+const Listing = require("./models/listing.js");
+const methodOverride = require("method-override");
 
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 main().then(()=>{
     console.log("connection to DB");
@@ -28,6 +31,11 @@ app.get("/listings",async (req,res)=>{
    const allListings = await listing.find({});
    res.render("./listings/index.ejs",{allListings});
 });
+//new route
+app.get("/listings/new",(req,res)=>{
+    res.render("./listings/new.ejs");
+});
+
 
 //show route
 app.get("/listings/:id",async (req,res)=>{
@@ -36,12 +44,34 @@ app.get("/listings/:id",async (req,res)=>{
     res.render("./listings/show.ejs",{Listing});
 });
 
-
-//new route
-app.get("/listing/new",(req,res)=>{
-    
+//create route 
+app.post("/listings",async (req,res)=>{
+    let newlisting =  new listing(req.body.listing);
+    await newlisting.save();
+    res.redirect("/listings");
 });
 
+//edit route
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/edit.ejs",{listing});
+});
+
+//update
+app.put("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//delete
+app.delete("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await listing.findByIdAndDelete(id);
+    console.log("deleted");
+    res.redirect("/listings");
+});
 // app.get("/testlisting",async (req,res)=>{
 //     let sampleListing = new listing(
 //         {
