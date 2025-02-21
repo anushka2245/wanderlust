8da034a303979fa20555b9b5f0c389fa8945d438
anushka kushwaha <anushkakushwaha2245@gmail.com>
2025-02-21 15:24:31 +0530

    main file

--- app.js
+++ app.js
@@ -1,95 +1,96 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const listing = require("./models/listing.js");
 const path = require("path");
 const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
 const ejsmate = require("ejs-mate");
 
-
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs",ejsmate);
+app.engine("ejs", ejsmate);
 
-const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
-main().then(()=>{
-    console.log("connection to DB");
-}).catch((err)=>{
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+main()
+  .then(() => {
+    console.log("Connection to DB established");
+  })
+  .catch((err) => {
     console.log(err);
-});
-async function main(){
-    await mongoose.connect(MONGO_URL);
+  });
 
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
-app.get("/",(req,res)=>{
-    res.send("Hello, i am root");
+app.get("/", (req, res) => {
+  res.send("Hello, I am root");
 });
 
-//index route
-app.get("/listings",async (req,res)=>{
-   const allListings = await listing.find({});
-   res.render("./listings/index.ejs",{allListings});
-});
-//new route
-app.get("/listings/new",(req,res)=>{
-    res.render("./listings/new.ejs");
+// Index route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings }); // Removed './'
 });
 
-
-//show route
-app.get("/listings/:id",async (req,res)=>{
-    let {id} = req.params;
-    const Listing = await listing.findById(id);
-    res.render("./listings/show.ejs",{Listing});
+// New route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs"); // Removed './'
 });
 
-//create route 
-app.post("/listings",async (req,res)=>{
-    let newlisting =  new listing(req.body.listing);
-    await newlisting.save();
-    res.redirect("/listings");
+// Show route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id); // Renamed `Listing` to `listing`
+  res.render("listings/show.ejs", { listing }); // Removed './'
 });
 
-//edit route
-app.get("/listings/:id/edit",async(req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("./listings/edit.ejs",{listing});
+// Create route
+app.post("/listings", async (req, res) => {
+  let newlisting = new Listing(req.body.listing);
+  await newlisting.save();
+  res.redirect("/listings");
 });
 
-//update
-app.put("/listings/:id",async(req,res)=>{
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
+// Edit route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing }); 
 });
 
-//delete
-app.delete("/listings/:id",async(req,res)=>{
-    let {id}=req.params;
-    await listing.findByIdAndDelete(id);
-    console.log("deleted");
-    res.redirect("/listings");
+// Update route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
 });
-// app.get("/testlisting",async (req,res)=>{
-//     let sampleListing = new listing(
-//         {
-//         title:"my new villa",
-//         description:"its a beautiful villa",
-//         price:1000,
-//         location:"bangalore",
-//         image:"https://d3oo9u3p09egds.cloudfront.net/filters:format(webp)/rental_property/colina-villa-h/01_Facade__10_.jpeg",
-//         country:"India"
-//     }
-// );
-//     await sampleListing.save();
-//     console.log("listing saved");
-//     res.send("listing saved successful");
+
+// Delete route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  console.log("Deleted");
+  res.redirect("/listings");
+});
+
+// Sample listing test route (Uncomment if needed)
+// app.get("/testlisting", async (req, res) => {
+//   let sampleListing = new Listing({ // Changed 'listing' to 'Listing'
+//     title: "My New Villa",
+//     description: "It's a beautiful villa",
+//     price: 1000,
+//     location: "Bangalore",
+//     image: "https://d3oo9u3p09egds.cloudfront.net/filters:format(webp)/rental_property/colina-villa-h/01_Facade__10_.jpeg",
+//     country: "India",
+//   });
+//   await sampleListing.save();
+//   console.log("Listing saved");
+//   res.send("Listing saved successfully");
 // });
-app.listen(8080,()=>{
-    console.log("server is running on port 8080");
-})
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
